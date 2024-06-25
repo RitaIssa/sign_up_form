b235696b36a53da202721bf7bc377b17e1602050
RitaIssa <rita.issa@devy.com.au>
2024-06-25 11:51:37 -0500

    update code

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-document.addEventListener("DOMContentLoaded", function(){
+const error = document.querySelector("#error");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm_password");
 
-    let password = document.querySelector("#password").value;
-    let confirmPassword = document.querySelector("#confirm_password").value;
-
-    let error = document.querySelector("#error");
-
-    if(password == "" && confirmPassword == ""){
+password.addEventListener("keyup", e => {
+    if(password.value !== confirmPassword.value){
+        error.style.display = "inline-block";
         error.innerHTML = "* Passwords do not match";
     }
-    else if(password === confirmPassword){
-        error.innerHTML = "";
+})
+
+confirmPassword.addEventListener("keyup", e => {
+    if(password.value == confirmPassword.value){
+        error.style.display = "none";
+        document.querySelector('input[type="password"]:focus').style.outlineColor = "green";
     }
 })
+
+const clear = document.querySelector("button");
+const first = document.querySelector("#first_name");
+const last = document.querySelector("#last_name");
+const email = document.querySelector("#mail");
+const number = document.querySelector("#phone_number");
+const pass = document.querySelector("#password");
+const confirmP = document.querySelector("#confirm_password");
+
+clear.addEventListener("click", () => {
+  first.value = "";
+  last.value = "";
+  email.value = "";
+  number.value = "";
+  pass.value = "";
+  confirmP.value = "";
+});
